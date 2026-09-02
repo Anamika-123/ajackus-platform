b8eedfe0d12656e7d6ac5b9d0a0484ae08dbea28
Anamika Sharma <indorianamika@gmail.com>
2026-09-02 20:25:17 +0530

    Remove explicit ASC from event ordering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.order(start_date: :asc)
+    @events = Event.order(:start_at)
     @vote_counts = EventVote.where(event_id: @events).group(:event_id, :vote_type).count
   end
 end
